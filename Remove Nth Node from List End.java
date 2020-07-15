037afbcab5bd7a1fd67c2a6d66354046ad37f846
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-07-15 18:13:36 +0530

    Create Remove Nth Node from List End.java

---
+++ Remove Nth Node from List End.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode A, int B) {
+        if(A==null)
+        return null;
+        int i,len=0,a;
+        ListNode x = A;
+        while(x!=null)
+        {
+            len++;
+            x=x.next;
+        }
+        if(B>=len)
+        {
+            A = A.next;
+        }
+        else
+        {
+            ListNode y = A;
+            a = len-B;
+            for(i=0;i<a-1;i++)
+            {
+                y=y.next;
+            }
+            y.next=y.next.next;
+        }
+        return A;
+    }
+}
