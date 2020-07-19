a3a05e239d12d0ac419698af48c339910279fd51
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-07-19 19:14:27 +0530

    Create List Cycle.java

---
+++ List Cycle.java
@@ -0,0 +1,24 @@
+public class Solution {
+   public ListNode detectCycle(ListNode A) {
+        ListNode a = A;
+        ListNode slow = A;
+        ListNode fast = A;
+        while(fast!=null && fast.next!=null)
+        {
+            // System.out.println(slow.val+" "+fast.val);
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast)
+            break;
+        }
+        if(slow!=fast)
+        return null;
+        while(a!=slow)
+        {
+            // System.out.println(a.val+" "+slow.val);
+            a = a.next;
+            slow = slow.next;
+        }
+        return a;
+    }
+}
